f0227720a49c1f9b8f0abbba753c57ee2b65426a
girltalk <monkeymonkey@hanmail.net>
2017-02-07 23:49:51 +0900

    tett84

--- mltest.py
+++ mltest.py
@@ -12,6 +12,7 @@ W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.zeros([1]))
 y = W * x_data + b
 
+
 # Minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
